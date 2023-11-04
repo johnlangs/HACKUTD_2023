bd7a0f2b51354232258a878ba2df28d57efb494c
John Langs <johnslangs@tamu.edu>
2023-11-04 18:52:44 -0500

    Add buyer frontend state

--- src/View/MainPage/UserCalculator.jsx
+++ src/View/MainPage/UserCalculator.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Buyer from '../../lib/Buyer';
 import triangle from './UserCalculator.css';
 import { Fade, Paper, Card, Typography, Input, TextField, Grid } from '@mui/material';
 
 
 function UserCalculator() {
+  const [buyer, setBuyer] = useState(new Buyer);
+  const [approved, setApproved] = useState(true);
+  const [reasons, setReasons] = useState([]);
 
   return (
 
@@ -17,30 +21,75 @@ function UserCalculator() {
           <Typography variant='h3' sx={{ fontSize: '4.5vw' }}>
           I make roughly &nbsp;
           <Input 
+            onInput={(e) => {
+              let newBuyer = buyer;
+              newBuyer.income = Number(e.target.value);
+              setBuyer(newBuyer);
+              console.log(buyer);
+              let reasons = buyer.check();
+              setReasons(reasons);
+              setApproved(reasons.length === 0);
+            }}
             inputProps={{maxLength:10}} 
             sx={{ fontSize: '4.5vw', 
                   maxWidth: '10ch' }}
           /> 
            &nbsp;a year, and have&nbsp; 
           <Input 
+            onInput={(e) => {
+              let newBuyer = buyer;
+              newBuyer.savings = Number(e.target.value);
+              setBuyer(newBuyer);
+              console.log(buyer);
+              let reasons = buyer.check();
+              setReasons(reasons);
+              setApproved(reasons.length === 0);
+            }}
             inputProps={{maxLength:10}}  
             sx={{fontSize: '4.5vw', 
                   maxWidth: '10ch'}}
           /> 
            &nbsp;in savings. My FICO credit score is&nbsp;
-          <Input  
+          <Input 
+            onInput={(e) => {
+              let newBuyer = buyer;
+              newBuyer.credit = Number(e.target.value);
+              setBuyer(newBuyer);
+              console.log(buyer);
+              let reasons = buyer.check();
+              setReasons(reasons);
+              setApproved(reasons.length === 0);              
+            }} 
             inputProps={{maxLength:3}}
             sx={{fontSize: '4.5vw',
                   maxWidth: '3ch'}}
           />
            &nbsp;I expect to spend &nbsp;
           <Input 
+            onInput={(e) => {
+              let newBuyer = buyer;
+              newBuyer.mortgage = Number(e.target.value);
+              setBuyer(newBuyer);
+              console.log(buyer);
+              let reasons = buyer.check();
+              setReasons(reasons);
+              setApproved(reasons.length === 0);
+            }} 
             inputProps={{maxLength:10}}  
             sx={{fontSize: '4.5vw',
                   maxWidth: '10ch'}} 
           /> 
            &nbsp;a month on my mortgage payment, and I can put&nbsp; 
           <Input 
+            onInput={(e) => {
+              let newBuyer = buyer;
+              newBuyer.down_payment = Number(e.target.value);
+              setBuyer(newBuyer);
+              console.log(buyer);
+              let reasons = buyer.check();
+              setReasons(reasons);
+              setApproved(reasons.length === 0);
+            }} 
             sx={{fontSize: '4.5vw',
                   maxWidth: '10ch'}}
           /> 
