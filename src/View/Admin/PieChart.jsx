be2c890f9ba6b7d09a83ccb9245ee751a3027f6c
John Langs <johnslangs@tamu.edu>
2023-11-05 09:02:47 -0600

    Charts init

--- src/View/Admin/PieChart.jsx
+++ src/View/Admin/PieChart.jsx
@@ -1,38 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import {Chart} from 'react-google-charts';
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [    
+    ['Reason', 'Amount'],
+    ['CreditScore', 4028], 
+    ['ltv', 4748], 
+    ['dti', 7457], 
+    ['fedti', 5586],
+];
 
 export const options = {
-    title: "Break of rejection reasons",
+  title: "My Daily Activities",
 };
-  
 
-export default function PieChart() {
-    const [data, setData] = useState({});
-    const [loading, setLoading] = useState(true);
-
-    const getData = React.useCallback(async () => {
-        setData({'CreditScore': 4028, 'ltv': 4748, 'dti': 7457, 'fedti': 5586});
-        console.log(data);
-    }, []);
-
-    useEffect(() => {
-        setLoading(true);
-        getData();
-        setLoading(false);
-    }, [getData]);
-
-    return !loading ? (
-        <>
-            <Chart
-            chartType="PieChart"
-            data={data}
-            options={options}
-            width={"100%"}
-            height={"400px"}
-            />
-        </>
-    ) : (
-        <>
-        </>
-    );
+export function PieChart() {
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      maxheight={"400px"}
+    />
+  );
 }
